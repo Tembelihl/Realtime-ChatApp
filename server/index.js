1b18f255970ac74fba67482289fb39877ef65fc6
Tembelihle Biyana <lihlebiyana1@gmail.com>
2023-12-01 14:16:28 +0200

    was contunuing with the backend

--- server/index.js
+++ server/index.js
@@ -1,17 +1,40 @@
-const express = require("express")
-const cors = require("cors")
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const userRoute = require("./Routes/userRoute")
+require("dotenv").config();
 
-
-const app = express()
-require("dotenv").config()
-
-app.use(express.json())
-app.use(cors())
-
-const port = process.env.PORT || 5000;
+const app = express();
+const port = process.env.PORT || 5006;
 const uri = process.env.ATLAS_URI;
 
-app.listen(port, (req, res) =>{
-console.log(`Server running on port...: ${port}`);
+app.use(express.json());
+app.use(cors());
+app.use("/api/users", userRoute);
+
+app.get("/", (req, res) =>{
+    res.send("Wecome to my chatapp API..");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port: ${port}`);
+});
+
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 })
+  .then(() => console.log("MongoDB connection established"))
+  .catch((error) => console.error("MongoDB connection failed:", error.message));
+
+
+mongoose.connection.on('error', (err) => {
+  console.error('MongoDB connection error:', err);
+});
+
+mongoose.connection.on('disconnected', () => {
+  console.log('MongoDB disconnected');
+});
+
+
 
