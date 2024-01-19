fd62cb9e33888d38b7f2cf50319fe3d85f048cc5
Tembelihle Biyana <lihlebiyana1@gmail.com>
2024-01-19 15:00:47 +0200

    i created user file for backend

---
+++ api/models/user.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const userSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        reqiured:true
+    },
+    friendRequests:[
+        {
+            type:mongoose,Schema,Types,ObjectsId,
+            ref:"User"
+        }
+    ],
+    friends:[
+        {
+            type:mongoose,Schema,Types,ObjectsId,
+            ref:"User"
+        }
+    ],
+    sentFriendRequests:[
+        {
+            type:mongoose,Schema,Types,ObjectsId,
+            ref:"User"
+        },
+    ],
+});
+
+const User = mongoose.model("User",userSchema);
+module.exports = User
